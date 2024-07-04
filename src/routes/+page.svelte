1e2899b8acd16f74d8e8cbb17a4232b7549f9cf0
PandaGerrie <gert-jan@purplepanda.be>
2024-07-04 22:24:42 +0200

    - Redirection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
-  <script>
-    window.href="/projects";
-  </script>
-  <h1> Redsirecting </h1>
+<script context="module">
+  import { redirect } from '@sveltejs/kit';
+
+  export const load = () => {
+    throw redirect(302, '/projects');
+  };
+</script>
+
+<svelte:head>
+  <title>Redirecting...</title>
+  <meta http-equiv="refresh" content="0;url=/projects" />
+</svelte:head>
+
+  <h1> Redirecting... </h1>

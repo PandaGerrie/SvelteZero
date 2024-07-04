47446e6f13fc5a2207f1eaea8860e45d14a079ef
PandaGerrie <gert-jan@purplepanda.be>
2024-07-04 21:16:15 +0200

    - b2

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import cloudflare from '@sveltejs/adapter-cloudflare';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: cloudflare(),
+        adapter: cloudflare({
+			// default options are shown
+			pages: '.svelte-kit/cloudflare/pages',
+			assets: '.svelte-kit/cloudflare/assets',
+			fallback: null
+		  }),
     }
 };
 

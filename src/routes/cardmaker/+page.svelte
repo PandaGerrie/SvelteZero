54ba823a9203617b21e844ad80c81e0fbe0cf46d
PandaGerrie <gert-jan@purplepanda.be>
2024-07-12 10:57:43 +0200

    - final v

---
+++ src/routes/cardmaker/+page.svelte
@@ -0,0 +1,16 @@
+<section>
+    <div class="container mx-auto">
+      
+      <div class="grid grid-cols-4 border-y-2 border-amber-50/[.1] max-[992px]:grid-cols-1">
+        <div class="col-span-1 align-start py-8 border-e-2 border-amber-50/[.1] sideBar max-[992px]:pb-0">
+            <p>Side 1</p>
+        </div>
+        <div class="col-span-2 px-10 py-8 contentBar">
+            <p>card creator</p>
+        </div>
+        <div class="col-span-1 align-start py-8 border-s-2 border-amber-50/[.1] sideBar max-[992px]:pb-0">
+            <p>Side 2</p>
+        </div>
+      </div>
+    </div>
+  </section>
